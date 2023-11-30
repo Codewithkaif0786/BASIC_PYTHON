3819e4c0466e45e883ed50e7ab7eba0d203d689c
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-30 10:40:56 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Multiply the two numbers
+result = num1 * num2
+
+# Display the result
+print(f"The result of multiplying {num1} and {num2} is: {result}")
